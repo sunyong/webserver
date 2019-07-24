fb0eae8bb4d3f202a3924ff4c442872a49c2dad6
sunyong <44562357+sunyong@users.noreply.github.com>
2019-07-24 13:55:46 +0900

    Create web.js

---
+++ web.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var app = express();
+
+
+/* serves all the static files */
+ app.get(/^(.+)$/, function(req, res){ 
+     console.log('static file request : ' + req.params);
+	 console.log("app get", req.params[0]);
+	 console.log("app get parameter", req.query.name);
+     res.sendfile( __dirname + req.params[0]); 
+ });
+
+ var port = process.env.PORT || 5000;
+console.log("port", port);
+
+ app.listen(port, function() {
+   console.log("Listening on " + port);
+ });
