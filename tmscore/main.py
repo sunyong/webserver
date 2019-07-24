9ff915849bd2fc8ce75498e310ab5775f46327b1
sunyong <44562357+sunyong@users.noreply.github.com>
2019-07-24 17:43:05 +0900

    Update main.py

--- tmscore/main.py
+++ tmscore/main.py
@@ -3,14 +3,15 @@ import Distributer
 from RouteFinder import RouteFinder
 
 def main():
-    DataController.loadDataFromCache()
+    print("a")
+    #DataController.loadDataFromCache()
     #DataController.loadData('data.txt')
-    Distributer.clustering()
-    DataController.storeTSPFile('data')
+    #Distributer.clustering()
+    #DataController.storeTSPFile('data')
 
-    finder = RouteFinder()
-    for fname in DataController.getTSPFilenames():
-        finder.route(fname)
+    #finder = RouteFinder()
+    #for fname in DataController.getTSPFilenames():
+        #finder.route(fname)
 
 if __name__ == "__main__":
     main()
